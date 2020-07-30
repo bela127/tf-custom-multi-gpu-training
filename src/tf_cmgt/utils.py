b3b3a3a18ca4f3bd9f2e561bec51bb9929a9e53d
inferics@DeepLearningBox.AnyUser <inferics@DeepLearningBox.AnyUser>
2020-07-30 02:06:35 +0200

    change auto_name_log_folder to allways ask

--- src/tf_cmgt/utils.py
+++ src/tf_cmgt/utils.py
@@ -1,7 +1,12 @@
 import datetime
 
 def auto_name_log_folder(log_folder, query_for_name = True):
-    if not log_folder:
+    if log_folder:
+        if query_for_name:
+            log_folder_new = input(f"input a log folder name or nothing to use {log_folder}:")           
+            if log_folder_new:
+                log_folder = log_folder_new
+    else:
         current_time = datetime.datetime.now()
         date_time = current_time.strftime("%d_%m_%Y-%H_%M_%S")
         if query_for_name:
